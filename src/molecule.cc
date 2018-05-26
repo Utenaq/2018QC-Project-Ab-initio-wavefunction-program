d3d9d714997acc0a957b932595adbc6adc12c50b
Zhi Zi <x@zzi.io>
2018-05-26 16:36:51 +0800

    fix typo

--- src/molecule.cc
+++ src/molecule.cc
@@ -29,11 +29,11 @@ Molecule::Molecule(int natom, int q, int *z, int **geom) {
 
 }
 
-Molecule(const char *z_matrix, int q) {
+Molecule::Molecule(const char *z_matrix, int q) {
 
 }
 
-Molecule(const char *z_matrix) {
+Molecule::Molecule(const char *z_matrix) {
 
 }
 
