b4b6cffa66962f394709c80c4a20911b239cef13
Zhi Zi <x@zzi.io>
2018-05-27 01:06:08 +0800

    basic IO for SCF

--- src/basic_SCF.h
+++ src/basic_SCF.h
@@ -3,16 +3,27 @@
 
 #include "../include/molecule.h"
 
+// read single electron integrals from files
+Matrix integrals_preprocess_S(const char *S_filename);
+Matrix integrals_preprocess_T(const char *T_filename);
+Matrix integrals_preprocess_V(const char *V_filename);
+
+// read 2 electron integrals from file
+Matrix integrals_preprocess_ERI(const char *ERI_filename);
+
+// read initial guess, if any
+Matrix read_initial_guess(const char *initial_guess_filename);
+
 // build the symmetric orthogonalization matrix S^(1/2) by diagnalizing the
 // overlap matrix S
-Matrix S12(Matrix S);
+Matrix calc_S_ortho(Matrix S);
 
 // initialize Fock matrix with default params (currently just H_{core})
 Matrix initialize_guess();
 Matrix initialize_guess(Matrix init_guess);
 
 // calculate density matrix
-Matrix calc_density_matrix(Matrix fock_matrix);
+Matrix calc_density_matrix(Matrix fock_matrix, Matrix core_hamiltonian, Matrix S_ortho);
 
 double calc_energy_elec(Matrix density_matrix, Matrix fock_matrix, Matrix core_hamiltonian);
 
@@ -23,5 +34,6 @@ Matrix calc_fock_matrix(Matrix core_hamiltonian, Matrix density_matrix, Tensor E
 
 double test_convergence(double etot_previous, double etot_current);
 
+void print_output(bool verbosity);
 
-#endif
+#endif // header guard
